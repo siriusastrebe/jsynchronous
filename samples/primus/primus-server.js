ef36ec1fa8aa255ebd0fe3326dd61889b2d77cf5
sirius strebe <siriusastrebe@gmail.com>
2021-06-16 01:17:40 -0700

    Primus sample server working.

--- samples/primus/primus-server.js
+++ samples/primus/primus-server.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const Primus = require('primus');
 const WebSocket = require('ws');
 const jsynchronous = require('../../jsynchronous.js');
 
-const wss = new WebSocket.Server({ port: 8080 });
-
 jsynchronous.send = (websocket, data) => {
   console.log(`${(data.length/1000).toFixed(3)} kB`);   
-  websocket.send(data);
+  websocket.write(data);
 }
 
 const physics = jsynchronous({position: [2, 0, 0], velocity: [0, 0, 0], acceleration: [0.001, -0.008, 0.0002]})
@@ -21,26 +20,29 @@ setInterval(() => {
   physics.position[2] += physics.velocity[2];
 }, 1000);
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-
-  physics.$sync(ws);
-});
+// Primus websocket server
+const primus = Primus.createServer(function connection(spark) {
+  physics.$sync(spark);
+}, { port: 8080, transformer: 'websockets' });
 
 // Express fileserver
 const app = express();
 const port = 3000;
 const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
 
 // Routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-})
+});
 
 app.get('/jsynchronous-client.js', (req, res) => {
   res.sendFile('/jsynchronous-client.js', {'root': '../../'});
-})
+});
+
+app.get('/primus/primus.js', (req, res) => {
+  res.setHeader('content-type', "application/javascript");
+  res.type("application/javascript");
+  res.send(primus.library());
+});
