0210ce964a55457d47cab2a6bad0e8994e5fea8d
sirius strebe <siriusastrebe@gmail.com>
2021-07-10 17:55:43 -0700

    Scaffolding selenium test framework.

--- tests/selenium/app.js
+++ tests/selenium/app.js
@@ -12,16 +12,11 @@ const port = 3000;
 // Jsynchronous
 jsynchronous.send = (websocket, data) => {
   websocket.emit('msg', data);
-  console.log(`${(data.length/1000).toFixed(2)} kB`);   
+  // console.log(`${(data.length/1000).toFixed(2)} kB`);   
 }
 
-const physics = {velocity: {x: 5, y: -2.04}};
-const $ynchronized = jsynchronous(physics);
-
-setInterval(() => {
-  $ynchronized.velocity.x += 5;
-  $ynchronized.velocity.y -= 9.81;
-}, 1000);
+let level = 0;
+const $test = jsynchronous({level: 0});
 
 // Express
 const server = app.listen(port, () => {
@@ -32,9 +27,9 @@ const server = app.listen(port, () => {
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-  $ynchronized.$ync(socket);  
+  $test.$ync(socket);  
   socket.on('msg', (data) => jsynchronous.onmessage(socket, data));
-  socket.on('disconnect', () => $ynchronized.$unsync(socket));
+  socket.on('disconnect', () => $test.$unsync(socket));
 });
 
 // Routes
@@ -46,18 +41,48 @@ app.get('/jsynchronous-client.js', (req, res) => {
   res.sendFile('/jsynchronous-client.js', {'root': '../../'});
 });
 
-
 // Selenium
 (async () => {
   let driver = await new Builder().forBrowser('firefox').build();
   try {
     await driver.get('http://localhost:3000');
-    const body = await driver.findElement(By.css('body'));
-    await driver.wait(until.elementTextContains(body, ''), 10000);
-    console.log(await body.getText());
+    await test(driver, 'Creation of a standard jsynchronous object', $test);
+    console.log(`All tests passed!`);
   } catch (e) {
     console.error(e);
   } finally {
     await driver.quit();
   }
 })();
+
+async function test(driver, text, $ynced) {
+  if (text) {
+    level++;
+    console.log(`Level ${level} - ${text}`);
+  }
+  await driver.wait(waitUntilLevel(driver, level), 8000);
+
+  /*
+  await driver.wait(() => {
+    await driver.executeScript('return jsynchronous()')
+    $ynced.
+  }), 8000);
+  */
+}
+
+async function waitUntilLevel(driver, level) {
+  const $levels = await driver.executeScript("return jsynchronous('object')");
+  if ($levels && $levels.level === level) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// ----------------------------------------------------------------
+// Helper functions
+// ----------------------------------------------------------------
+function randomHash() {
+  // A random hash with random length
+  return Math.random().toString(36).substring(2);
+}
